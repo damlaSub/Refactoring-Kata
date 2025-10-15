269e0e84be450cd3207fab4b2e382333d2e44494
damlaSub <mme.dalloway@gmail.com>
2025-10-15 13:00:34 +0200

    add tests for requirements

--- GildedRose-Refactoring-Kata/Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose-Refactoring-Kata/Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,13 +5,98 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
+    // All items have SellIn (days to sell)
+    // @Test
+    void allItems_haveSellInValueThatDecreasesBy1EachDay() {
+
+        Item item = new Item("+5 Dexterity Vest", 10, 20);
+        GildedRose app = new GildedRose(new Item[]{item});
+        app.updateQuality();
+        assertEquals(9, item.sellIn, "SellIn should decrease by 1 each day");
+    }
+
+    // All items have Quality value (value/condition)
+    @Test
+    void allItems_haveQualityValueThatChangesDaily() {
+
+        Item item = new Item("+5 Dexterity Vest", 10, 20);
+        GildedRose app = new GildedRose(new Item[]{item});
+        app.updateQuality();
+        assertEquals(19, item.quality, "Quality should decrease by 1 each day for normal items");
+    }
+
+    // Once the sell by date has passed, Quality degrades twice as fast
+    @Test
+    void normalItem_qualityDegradesTwiceAsFastAfterSellDate() {
+
+        Item item = new Item("+5 Dexterity Vest", 0, 10);
+        GildedRose app = new GildedRose(new Item[]{item});
+        app.updateQuality();
+        assertEquals(8, item.quality, "Quality should drop by 2 after sell date passes");
+    }
+
+    // "Aged Brie" actually increases in Quality the older it gets
+    @Test
+    void agedBrie_increasesInQualityEachDay() {
+        Item item = new Item("Aged Brie", 10, 10);
+        GildedRose app = new GildedRose(new Item[]{item});
+        app.updateQuality();
+        assertEquals(9, item.sellIn);
+        assertEquals(11, item.quality, "Aged Brie should increase in quality by 1 per day");
+    }
+
+    // The Quality of an item is never more than 50
+    @Test
+    void qualityNeverExceeds50() {
+        Item item = new Item("Aged Brie", 5, 50);
+        GildedRose app = new GildedRose(new Item[]{item});
+        app.updateQuality();
+        assertEquals(50, item.quality, "Quality should never exceed 50");
+    }
+
+    // "Sulfuras" never has to be sold or decreases in Quality
+    @Test
+    void sulfuras_neverDecreasesInSellInOrQuality() {
+        Item item = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+        GildedRose app = new GildedRose(new Item[]{item});
+        app.updateQuality();
+        assertEquals(0, item.sellIn);
+        assertEquals(80, item.quality);
+    }
+
+    // "Backstage passes" increases in Quality as it approaches SellIn
+    @Test
+    void backstage_increasesBy1WhenMoreThan10Days() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+        GildedRose app = new GildedRose(new Item[]{item});
+        app.updateQuality();
+        assertEquals(14, item.sellIn);
+        assertEquals(21, item.quality, "Quality should increase by 1 when sellIn > 10");
+    }
+    @Test
+    void backstage_increasesBy2When10DaysOrLess() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 25);
+        GildedRose app = new GildedRose(new Item[]{item});
+        app.updateQuality();
+        assertEquals(9, item.sellIn);
+        assertEquals(27, item.quality, "Quality should increase by 2 when sellIn <= 10");
+    }
+    @Test
+    void backstage_increasesBy3When5DaysOrLess() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 25);
+        GildedRose app = new GildedRose(new Item[]{item});
+        app.updateQuality();
+        assertEquals(4, item.sellIn);
+        assertEquals(28, item.quality, "Quality should increase by 3 when sellIn <= 5");
+    }
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void backstage_qualityDropsToZeroAfterConcert() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 25);
+        GildedRose app = new GildedRose(new Item[]{item});
         app.updateQuality();
-        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(-1, item.sellIn);
+        assertEquals(0, item.quality, "Quality should drop to 0 after the concert (sellIn < 0)");
     }
 
 }
